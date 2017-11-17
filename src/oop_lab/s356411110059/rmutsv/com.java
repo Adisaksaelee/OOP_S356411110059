8ae79f1907cbe58b3ece1f5a15cd20d1327f9816
Adisak <sunping66@hotmail.com>
2017-11-17 10:33:31 +0700

    Initial commit

---
+++ src/oop_lab/s356411110059/rmutsv/com.java
@@ -0,0 +1,4 @@
+package oop_lab.s356411110059.rmutsv;
+
+public class com {
+}
---
+++ src/oop_lab/s356411110059/rmutsv/myFirstJAVA.java
@@ -0,0 +1,8 @@
+package oop_lab.s356411110059.rmutsv;
+
+public class myFirstJAVA {
+    public static void main(String[] args) {
+        System.out.println("Hello Wordl,I'am SUNPHING");
+    }
+}
+
