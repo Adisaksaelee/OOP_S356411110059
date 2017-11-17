b4f984b6508b30765831322509edaf943bfe47cc
Adisak <sunping66@hotmail.com>
2017-11-17 11:14:19 +0700

    successfuly information data

--- src/oop_lab/s356411110059/rmutsv/myFirstJAVA.java
+++ src/oop_lab/s356411110059/rmutsv/myFirstJAVA.java
@@ -3,6 +3,8 @@ package oop_lab.s356411110059.rmutsv;
 public class myFirstJAVA {
     public static void main(String[] args) {
         System.out.println("Hello Wordl,I'am SUNPHING");
+        System.out.println("information System");
+        System.out.println("Faculty of Mangament Technololgy");
+        System.out.println("RMUTSV");
     }
 }
-
